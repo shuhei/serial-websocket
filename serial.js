632668ed2206c58df352165e1b767ff7d2991900
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2011-11-13 19:13:06 +0900

    Serve client code with express and eliminated multi-serial connections which have caused strange behaviours.

--- serial.js
+++ serial.js
@@ -1,23 +1,75 @@
-var sys = require('sys')
-  , serialport = require('serialport')
+var serialport = require('serialport')
   , SerialPort = serialport.SerialPort
+  , child = require('child_process')
+  , express = require('express')
+  , app = express.createServer()
+  , io = require('socket.io').listen(app)
 ;
 
-var io = require('socket.io').listen(4649);
+// Simple Array Remove inspired John Resig
+// http://ejohn.org/blog/javascript-array-remove/
+Array.prototype.remove = function(i) {
+  var sliced = this.slice(i + 1);
+  this.length = i;
+  // Use apply to push multiple values in an array.
+  this.push.apply(this, sliced);
+};
 
+app.listen(3000);
+
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.set('view options', { layout: false });
+
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/', function(req, res) {
+  res.render('index', { socketHost: req.header('Header') });
+});
+
+var sockets = [];
 io.sockets.on('connection', function(socket) {
-  var serial = new SerialPort('/dev/tty.usbmodemfa131', {
+  sockets.push(socket);
+  io.sockets.on('diconnect', function() {
+    for (var i = 0; i < sockets.length; i++) {
+      if (sockets[i] === socket) {
+        sockets.remove(i);
+        break;
+      }
+    }
+  });
+});
+
+child.exec('ls /dev/tty.usbmodem*', function(err, stdout, strerr) {
+  var usbs = stdout.split('\n');
+  if (usbs && usbs.length > 0) {
+    startSerial(usbs[0]);
+  } else {
+    console.log('No USB device connected.');
+    process.exit(1);
+  }
+});
+
+function startSerial(path) {
+  var serial = new SerialPort(path, {
       baudrate: 9600
     , parser: serialport.parsers.readline()
   });
-  
+  console.log('Connected to ' + path);
+
   serial.on('data', function(chunk) {
     // chunk is a Buffer object.
     var sent = chunk.toString().trim();
-    socket.emit('serial', sent);
+    for (var i = 0; i < sockets.length; i++) {
+      sockets[i].emit('serial', sent);
+    }
   });
-
+  
   serial.on('error', function(message) {
-    sys.puts('error: ' + message);
+    console.log('error: ' + message);
   });
-});
+}
+
